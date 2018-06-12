833de71b7e793b1e5659078bc83f7c4849598e10
Victor Vasquez <victor.vasquez96@unitec.edu>
2018-06-12 00:23:11 -0600

    Fixed main menu

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from src.fibonacci_dp import Fibonacci
 
 def main():
     print ("Dijkstra\n")
-    fibo = input("Number for fibonacci : ")
-    print "Fibonacci of : ", fibo, " is " ,Fibonacci(fibo), "\n"
     print ("Bellman Ford\n")
     print ("Fibonaccio Dynamic Programming way\n")
+    fibo = input("Number for fibonacci : ")
+    print "Fibonacci of : ", fibo, " is " ,Fibonacci(fibo), "\n"
 
 if __name__ == "__main__":
     main()
